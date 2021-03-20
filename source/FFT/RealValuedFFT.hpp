c2d95a5f0751f8ad5ef5d44b0ceca9a2f5cd9b87
Lorenz Bucher <lorenz.bucher@gmail.com>
2021-03-20 16:49:36 +0100

    extended tests

--- source/FFT/RealValuedFFT.hpp
+++ source/FFT/RealValuedFFT.hpp
@@ -20,7 +20,6 @@ extern "C"
 #include "DSPF_sp_ifftSPxSP_cn.h"
 #include "TI_FFT_support.h"
 }
-//#include "FFTReal.hpp"
 
 namespace slb {
 
@@ -33,23 +32,29 @@ public:
         m_splitTableB(fftLength/2),
         m_twiddleTable(fftLength/2)
     {
+        // TODO: power of 2
         const int N = fftLength/2;
         tw_gen(reinterpret_cast<float*>(&m_twiddleTable[0]), N);
         split_gen(reinterpret_cast<float*>(&m_splitTableA[0]), reinterpret_cast<float*>(&m_splitTableB[0]), N);
         
-        if (N == 4 || N == 16 || N == 64 || N == 256 || N == 1024 || N == 4096 || N == 16384) {
+        if (N == 16 || N == 64 || N == 256 || N == 1024 || N == 4096 || N == 16384) {
             m_radix = 4;
         } else if (N == 8 || N == 32 || N == 128 || N == 512 || N == 2048 || N == 8192) {
             m_radix = 2;
         } else {
             ASSERT_ALWAYS("Length not supported");
         }
+
+        tw_gen(reinterpret_cast<float*>(&m_twiddleTable[0]), N);
+        split_gen(reinterpret_cast<float*>(&m_splitTableA[0]), reinterpret_cast<float*>(&m_splitTableB[0]), N);
     }
     
     /** Calculates the FFT for a real-valued input - using a split-complex FFT */
-    std::vector<std::complex<float>> perform(const std::vector<float>& realInput)
+    std::vector<std::complex<float>> performForward(const std::vector<float>& realInput)
     {
         ASSERT(realInput.size() == m_fftLength, "Signal length must match FFT Size");
+        // TODO: zero-pad
+        
         // Trick: We calculate a complex FFT of length N/2  ('split complex FFT')
         const int N = m_fftLength / 2;
         
@@ -67,7 +72,7 @@ public:
         DSPF_sp_fftSPxSP(N, (float*)&pseudoComplexInput[0], (float*)&m_twiddleTable[0], (float*)&complexOutput[0],
                          (unsigned char*) brev.data(), m_radix, offset, N);
 
-        std::vector<std::complex<float>> result(m_fftLength+1);
+        std::vector<std::complex<float>> result(m_fftLength+1); // entire length +1 required for calculation 
         FFT_Split(N, (float*)&complexOutput[0], (float*)&m_splitTableA[0], (float*)&m_splitTableB[0], (float*)&result[0]);
 
         // TODO: make buffers members so there's no allocation
@@ -75,71 +80,32 @@ public:
         return {result.begin(), result.begin() + N+1}; // only return fftLength/2+1 complex pairs
     }
     
-private:
-    /**
-     * Split the result to obtain a 2*N =fftLength complex sequence
-     *
-     * From TI's spra291:
-     * @code
-     *  X(N) = X(0)
-     *  for k=0 .. N-1
-     *      Gr(k) = Xr(k) Ar(k) – Xi(k) Ai(k) + Xr(N–k) Br(k) + Xi(N–k) Bi(k)
-     *      Gi(k) = Xi(k) Ar(k) + Xr(k) Ai(k) + Xr(N–k) Bi(k) – Xi(N–k) Br(k)
-     *      Gr(2N–k) = Gr(k)
-     *      Gi(2N–k) = –Gi(k)
-     *  Gr(N) = Gr(0) – Gr(1)
-     *  Gi(N) = 0
-     * @endcode
-     */
-    std::vector<std::complex<float>> split(std::vector<std::complex<float>>& splittableSequence)
+    std::vector<float> performInverse(const std::vector<std::complex<float>>& complexInput)
     {
-        const int N = m_fftLength / 2;
-        ASSERT(splittableSequence.size() == N, "Input has wrong length");
-        std::vector<std::complex<float>> result(m_fftLength);
+        const int N = m_fftLength/2;
+        std::vector<std::complex<float>> tempComplexBuffer(N+1);
         
-        // aliases
-        decltype(splittableSequence)& X = splittableSequence;
-        decltype(m_splitTableA)& A = m_splitTableA;
-        decltype(m_splitTableB)& B = m_splitTableB;
+        // TODO: casts
+        IFFT_Split(N, (float*)&complexInput[0], (float*)&m_splitTableA[0], (float*)&m_splitTableB[0], (float*)&tempComplexBuffer[0]);
         
-        X[N] = X[0];
-        for (int k = 0; k < N; ++k) {
-            float real = X[k].real()   * A[k].real() - X[k].imag()   * A[k].imag()
-                       + X[N-k].real() * B[k].real() + X[N-k].imag() * B[k].imag();
-            
-            float imag = X[k].imag()   * A[k].real() + X[k].real()   * A[k].imag()
-                       + X[N-k].real() * B[k].imag() - X[N-k].imag() * B[k].real();
-            
-            result[k] = { real, imag };
-            
-            // Use complex conjugate symmetry properties for the second half
-            result[2*k] = std::conj(result[k]);
-        }
-        result[N] = { result[0].real() - result[1].real(), 0.f };
+        std::vector<float> result(m_fftLength);
+        const int offset = 0;
+        
+        // Inverse FFT Calculation using N/2 complex IFFT
+        DSPF_sp_ifftSPxSP(N, (float*)&tempComplexBuffer[0], (float*)&m_twiddleTable[0], (float*)&result[0],
+                          (unsigned char*) brev.data(), m_radix, offset, N);
+        
+        // TODO: make buffers members so there's no allocation
         return result;
-        
     }
     
 private:
     int m_fftLength;
+    int m_radix;
     
     std::vector<std::complex<float>> m_splitTableA;
     std::vector<std::complex<float>> m_splitTableB;
     std::vector<std::complex<float>> m_twiddleTable;
-    
-    int m_radix;
-
 };
-
-class RealValuedIFFT
-{
-public:
-    RealValuedIFFT(int fftLength) {}
-    
-    void process(float* input, float** complex);
-private:
-    
-};
-
 
 } // namespace slb
---
+++ test/tests/RealValuedFFTTests.cpp
@@ -0,0 +1,168 @@
+//
+//  ╔═╗┬ ┬┌┬┐┬┌─┐╔╦╗┬─┐┌─┐┬┌┬┐┌─┐
+//  ╠═╣│ │ ││││ │ ║ ├┬┘├─┤│ │ └─┐
+//  ╩ ╩└─┘─┴┘┴└─┘ ╩ ┴└─┴ ┴┴ ┴ └─┘
+//
+//  © 2021 Lorenz Bucher - all rights reserved
+
+#include "TestCommon.hpp"
+
+#include <numeric>
+#include <iostream>
+
+#include "FFT/RealValuedFFT.hpp"
+
+using namespace slb;
+using namespace TestCommon;
+using namespace std::complex_literals;
+
+std::vector<float> calculateNormalizedMagnitude(std::vector<std::complex<float>> binData)
+{
+    int numSamples = (static_cast<int>(binData.size()) - 1) * 2;
+    std::vector<float> magnitude;
+    for (auto& binValue : binData) {
+        magnitude.push_back(std::abs(binValue) / numSamples);
+    }
+    float maxValue = *std::max_element(magnitude.begin(), magnitude.end());
+    maxValue = std::max(1e-9f, maxValue); // avoid div by zero
+    for (auto& e : magnitude) { e /= maxValue; }
+    return magnitude;
+}
+
+double calculateStdDev(std::vector<float> data)
+{
+    double sum = std::accumulate(data.begin(), data.end(), 0.0);
+    double mean = sum / data.size();
+
+    double sq_sum = std::inner_product(data.begin(), data.end(), data.begin(), 0.0);
+    return std::sqrt(sq_sum / data.size() - mean * mean);
+}
+
+TEST_CASE("RealValuedFFT Tests")
+{
+    int N = GENERATE(16, 512, 4096, 16384); // TODO: 32k does not work??
+    const int numBins = N/2 + 1;
+    RealValuedFFT fft(N);
+    
+    SECTION("Forward Transform") {
+    
+        SECTION("Zeros / no signal") {
+            std::vector<float> zeros(N, 0);
+            std::vector<std::complex<float>> result = fft.performForward(zeros);
+
+            REQUIRE(result.size() == numBins);
+            std::vector<std::complex<float>> expected(numBins, {0, 0});
+            REQUIRE(std::equal(result.begin(), result.end(), expected.begin()));
+            
+            std::vector<float> magnitude = calculateNormalizedMagnitude(result);
+            REQUIRE(magnitude == std::vector<float>(numBins, 0.f));
+        }
+        SECTION("Dirac Input") {
+            std::vector<float> dirac = createDirac<float>(N);
+            std::vector<std::complex<float>> result = fft.performForward(dirac);
+            
+            REQUIRE(result.size() == numBins);
+            std::vector<std::complex<float>> expected(numBins, {1, 0});
+            REQUIRE(std::equal(result.begin(), result.end(), expected.begin(), [](auto& a, auto& b)
+            {
+                return std::abs(a-b) < 1e-6f;
+            }));
+            
+            std::vector<float> magnitude = calculateNormalizedMagnitude(result);
+            REQUIRE(magnitude == std::vector<float>(numBins, 1.f));
+        }
+        SECTION("Sine Input") {
+            float sampleRate = 48e3f;
+            float frequency = GENERATE(1000, 200, 10000, 24e3);
+            std::vector<float> sine = createSine<float>(frequency, sampleRate, N);
+            std::vector<std::complex<float>> result = fft.performForward(sine);
+
+            REQUIRE(result.size() == numBins);
+            std::vector<float> magnitude = calculateNormalizedMagnitude(result);
+
+            int expectedBin = static_cast<int>(std::round(frequency / sampleRate * N));
+            REQUIRE(magnitude[expectedBin] == 1.f); //normalization guarantees this
+
+            // other bins are more than 4dB below
+            for (int binIndex = 0; binIndex < numBins; ++binIndex) {
+                if (binIndex == expectedBin) continue;
+                REQUIRE(magnitude[binIndex] < Utils::dB2Linear(-4.f));
+            }
+            if (N >= 512) {
+                REQUIRE(calculateStdDev(magnitude) <= 0.075f);
+            }
+        }
+        SECTION("Noise Input") {
+            std::vector<float> noise = createRandomVector(N);
+            std::vector<std::complex<float>> result = fft.performForward(noise);
+ 
+            REQUIRE(result.size() == numBins);
+            
+            if (N >= 512) {
+                std::vector<float> magnitude = calculateNormalizedMagnitude(result);
+                REQUIRE(calculateStdDev(magnitude) <= 0.18f);
+            }
+        }
+    }
+    SECTION("Inverse Transform") {
+        std::vector<std::complex<float>> zeros(numBins, {0, 0});
+        std::vector<float> result = fft.performInverse(zeros);
+        
+        REQUIRE(result.size() == N);
+        std::vector<float> expected(N, 0);
+        REQUIRE(std::equal(result.begin(), result.end(), expected.begin()));
+    }
+    
+    SECTION("Chain of FFT and IFFT") {
+        std::vector<std::complex<float>> freqDomainBuffer(numBins);
+        std::vector<float> timeDomainBuffer(N);
+        
+        std::vector<float> noise = createRandomVector(N);
+        std::vector<std::complex<float>> result = fft.performForward(noise);
+
+        std::vector<float> restoredNoise = fft.performInverse(result);
+        
+        REQUIRE(std::equal(noise.begin(), noise.end(), restoredNoise.begin(), [](auto& a, auto& b)
+        {
+            return std::abs(a-b) < 1e-6f;
+        }));
+    }
+
+    SECTION("Numeric Example: Ramp Signal") {
+        
+        if (N == 16) {
+            std::vector<float> testSequence { 0.062500, 0.125000, 0.187500, 0.250000, 0.312500, 0.375000, 0.437500, 0.500000,
+                                            0.562500, 0.625000, 0.687500, 0.750000, 0.812500, 0.875000, 0.937500, 1.000000 };
+
+            REQUIRE(testSequence.size() == N);
+            
+            std::vector<std::complex<float>> result = fft.performForward(testSequence);
+        
+            // Results from FFT in Matlab - (only from DC until Nyquist frequency)
+            const std::vector<std::complex<float>> reference {
+                 8.5000f + 0.0000if,
+                -0.5000f + 2.5137if,
+                -0.5000f + 1.2071if,
+                -0.5000f + 0.7483if,
+                -0.5000f + 0.5000if,
+                -0.5000f + 0.3341if,
+                -0.5000f + 0.2071if,
+                -0.5000f + 0.0995if,
+                -0.5000f + 0.0000if
+            };
+            REQUIRE(reference.size() == numBins);
+            
+            REQUIRE(std::equal(result.begin(), result.end(), reference.begin(), [](auto a, auto b)
+            {
+                return std::abs(a-b) < 5e-4f;
+            }));
+            
+            std::vector<float> restoredSequence = fft.performInverse(result);
+        
+            REQUIRE(std::equal(restoredSequence.begin(), restoredSequence.end(), testSequence.begin(), [](auto a, auto b)
+            {
+                return std::abs(a-b) < 5e-4f;
+            }));
+        }
+    }
+}
