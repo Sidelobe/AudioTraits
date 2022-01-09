f0028181dc45800bc8f296de41e4aa92f73f1a16
Lorenz Bucher <lorenz.bucher@gmail.com>
2022-01-09 16:27:02 +0100

    comments

--- source/ChannelSelection.hpp
+++ source/ChannelSelection.hpp
@@ -26,8 +26,9 @@ public:
         ASSERT(channel > 0, "invalid channel!");
     }
     
-    /** Range
-     TODO: consider making this explicit -- maybe rename class to something shorter in this case
+    /**
+     * Range of channels (deliberately non-explicit ctor)
+     * TODO: consider making this explicit -- maybe rename class to something shorter in this case
      */
     SelectionItem(int first, int last) : m_selection(last - first + 1)
     {
